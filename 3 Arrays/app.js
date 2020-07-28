9ee17b52d0481840ebbf75084cb8c92697ca65ee
Michael <mioncgp@gmail.com>
2020-07-28 12:40:47 +0400

    d

---
+++ 3 Arrays/app.js
@@ -0,0 +1,3 @@
+// There are two types of arrays Dynamic and Static
+// Static arrays fixed in size, the number of elements must be specified ahead of time
+// Dynamic arrays allow us to copy and rebuild an array at a new location in memory
