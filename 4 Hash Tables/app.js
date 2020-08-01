ab07ac2317df98f19b64feb9d4874aefe8cb2bbe
Michael <mioncgp@gmail.com>
2020-08-01 09:57:40 +0400

    done

--- 4 Hash Tables/app.js
+++ 4 Hash Tables/app.js
@@ -22,7 +22,6 @@ class HashTable {
       this.data[address] = [];
     }
     this.data[address].push([key, value]);
-    console.log(this.data);
     return this.data;
   }
 
@@ -39,4 +38,43 @@ class HashTable {
     }
     return undefined;
   }
+
+  // return keys
+  keys() {
+    const keysArray = [];
+    for (let i = 0; i < this.data.length; i++) {
+      if (this.data[i]) {
+        console.log(this.data);
+        keysArray.push(this.data[i][0][0]);
+      }
+    }
+    return keysArray;
+  }
+}
+
+// return the first recurring number
+// O(n^2)
+function firstRuccerNum(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] === arr[j]) {
+        return arr[i];
+      }
+    }
+  }
+  return undefined;
+}
+
+// O(n)
+function firstRecurNum2(arr) {
+  let map = {};
+
+  for (let i = 0; i < arr.length; i++) {
+    if (map[arr[i]] !== undefined) {
+      return arr[i];
+    } else {
+      map[arr[i]] = i;
+    }
+  }
+  return undefined;
 }
