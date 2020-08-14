f884994cbe0a81f4c47881c5fef0329934d1e2f0
Michael <mioncgp@gmail.com>
2020-08-14 08:46:09 +0400

    done

---
+++ 8 Graphs/app.js
@@ -0,0 +1,4 @@
+// In computer science, a graph is an abstract data type that is meant to implement the undirected graph and directed graph concepts from the field of graph theory within mathematics.
+// there are directed and undirected graphs
+// there are unweighted and weighted graphs
+// there are Cyclic and Acyclic
